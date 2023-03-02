a20bb5e90a276bad695f83fd9f0912c448c23417
hs96wings <hs96wings@gmail.com>
2023-03-02 09:28:35 +0900

    2단계: ëª: 명언 하나 등록하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s;
+        String s, quote, author;
+        int id = 0;
 
         System.out.println("== 명언 앱 ==");
         while (true) {
@@ -14,6 +15,13 @@ public class Main {
 
             if (s.equals("종료")) {
                 break;
+            } else if (s.equals("등록")) {
+                System.out.print("명언 : ");
+                quote = scanner.nextLine();
+                System.out.print("작가 : ");
+                author = scanner.nextLine();
+            } else {
+                System.out.println("잘못된 명령입니다");
             }
         }
     }
