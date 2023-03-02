c80995305d127be32a53d18dc4e95b598e89e0e3
hs96wings <hs96wings@gmail.com>
2023-03-02 09:23:49 +0900

    1단계: 실행 종료 만들기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,20 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String s;
+
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.print("명령) ");
+            s = scanner.nextLine();
+
+            if (s.equals("종료")) {
+                break;
+            }
+        }
     }
 }
