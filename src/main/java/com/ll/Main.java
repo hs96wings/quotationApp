ab7274e535ef1098c3b306b4bd1c30a122bf8f4c
hs96wings <hs96wings@gmail.com>
2023-03-02 09:32:26 +0900

    3단계 : 등록시 명언번호 표시

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,12 +1,14 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s, quote, author;
-        int id = 0;
+        int id = 1;
 
         System.out.println("== 명언 앱 ==");
         while (true) {
@@ -20,9 +22,22 @@ public class Main {
                 quote = scanner.nextLine();
                 System.out.print("작가 : ");
                 author = scanner.nextLine();
+                System.out.println(id + "번 명령이 등록되었습니다.");
             } else {
                 System.out.println("잘못된 명령입니다");
             }
         }
+    }
+}
+
+class Quotation {
+    private int id;
+    private String quote;
+    private String author;
+
+    Quotation(int id, String quote, String author) {
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
     }
 }
